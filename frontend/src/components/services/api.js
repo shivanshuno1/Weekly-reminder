ecf20bb5919baa780806a27a7480f332420d5061
Shivanshu <shivanshur62@gmail.com>
2025-11-11 00:10:33 +0530

    changes in api.js to connect with backned too

--- frontend/src/components/services/api.js
+++ frontend/src/components/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://backend-3mpcglr3j-shivanshu-rawats-projects.vercel.app/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
